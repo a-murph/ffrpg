3115396450d90afa9a54630141d4d59c5e947559
Andrew Murphy <andrew.murphy@conduent.com>
2023-02-11 14:04:20 -0500

    Initial setup using Boilerplate

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const FFRPG = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ FFRPG.abilities = {
+  "str": "FFRPG.AbilityStr",
+  "dex": "FFRPG.AbilityDex",
+  "con": "FFRPG.AbilityCon",
+  "int": "FFRPG.AbilityInt",
+  "wis": "FFRPG.AbilityWis",
+  "cha": "FFRPG.AbilityCha"
+};
+
+FFRPG.abilityAbbreviations = {
+  "str": "FFRPG.AbilityStrAbbr",
+  "dex": "FFRPG.AbilityDexAbbr",
+  "con": "FFRPG.AbilityConAbbr",
+  "int": "FFRPG.AbilityIntAbbr",
+  "wis": "FFRPG.AbilityWisAbbr",
+  "cha": "FFRPG.AbilityChaAbbr"
+};
